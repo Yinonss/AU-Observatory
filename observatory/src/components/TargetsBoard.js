6cf27f322d3911d14f65f0de7397a534d48350c6
maor6 <maor.ovadia6@gmail.com>
2021-03-16 14:39:14 +0200

    Change name and over the TargetLIst

--- observatory/src/components/TargetsBoard.js
+++ observatory/src/components/TargetsBoard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Style from './Style.css'
-
+import Target from "./Target"
 /* This component define the plan's structure and display its targets. */
 
-export default function TargetInfo() {
+export default function TargetsBoard(props) {
 
     return (
         <div class="TargetInfo">
@@ -14,6 +14,9 @@ export default function TargetInfo() {
                 </tr>
             </table>
             <div class="list">
+                {
+                    props.allTargets.map(item => <Target name={item.name}/>)
+                }
             </div>
             <br />
             <button id="submit">Submit</button>
