3dedb6997c094d30b6df4916c3a32d5eed4a0ae9
maor6 <maor.ovadia6@gmail.com>
2021-03-16 14:40:02 +0200

    Make Target List

--- observatory/src/components/NewPlan.js
+++ observatory/src/components/NewPlan.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Style from './Style.css'
 import Editor from "./Editor";
-import TargetList from "./TargetList";
-
+import TargetsBoard from "./TargetsBoard";
+import Target from"./Target"
 /* This component containing Editor and TargetList components.
 *  It is activated after press on "NEW PLAN" button on the main menu.  */
 
 //TODO: Add targets to list ability
 
 export default function NewPlan() {
+
+    const allTargets = [{name: "venos"}, {name: "53568m"}]
+
+    const [targets, setTargets] = useState(allTargets)
+    const addTarget = (target)=> {
+        setTargets([...targets, target])
+    }
+
     return (
         <div className="container">
-            <td><Editor /></td>
-            <td><TargetList /></td>
+            <td><Editor addTarget={addTarget}/></td>
+            <td><TargetsBoard allTargets={targets} /></td>
         </div>
     );
 }
