439ca9deb899c05af9b6c02b3815e018168e7e9c
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-04-15 15:01:54 +0300

    Update observation schema

--- observatory/backend/Model/Plan.js
+++ observatory/backend/Model/Plan.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
 
-const plan = new Schema({
+var plan = new Schema({
     title : String,
-    //observations : [{type : ObjectId, ref: 'Observation'}]
+    observations : [{type : ObjectId, ref: 'Observation'}]
 })
 
 module.exports = mongoose.model('Plan', plan)
