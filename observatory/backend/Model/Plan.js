b8762fcfc904964a5be766d5a9ffdab6ab7c7d4c
maor6 <maor.ovadia6@gmail.com>
2021-05-19 19:50:40 +0300

    Fix the plan schema

--- observatory/backend/Model/Plan.js
+++ observatory/backend/Model/Plan.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
 
+
 const plan = new Schema({
     title: {type: String, required: true},
-    observations: [{type: ObjectId, ref: 'Observation'}]
-    // timestamp: true  // add field when it create
-}, {timestamps: {createdAt: 'created_at'}});
+    observations: []
+}, {timestamps: {createdAt: 'created_at'}});  // add field when it create
 
 module.exports = mongoose.model('Plan', plan)
