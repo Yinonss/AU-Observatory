46ad143946f3c9d482e4432b370bb2453e7647e4
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-07-26 23:57:23 +0300

    Update Observation mongoose model

--- observatory/backend/Model/Observation.js
+++ observatory/backend/Model/Observation.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const observation = new Schema({
-    id: {type: String, required: true},
-    name: {type: String, required: true},
-    ra: {type: Number, required: true},
-    dec: {type: Number, required: true},
+    id: {type: String, required: false},
+    name: {type: String, required: false},
+    ra: {type: Number, required: false},
+    dec: {type: Number, required: false},
     exposures: [{type: Number}],
     exposureTime: [{type: Number}],
     filter: [{type: String}],
     bin: [{type: String}],
-    start: {type: String, required: true},
-    end: {type: String, required: true},
+    start: {type: String, required: false},
+    end: {type: String, required: false},
     repeat: {type: Number, required: false},
-    calibrate: {type: Boolean, required: true, default: false},
+    calibrate: {type: Boolean, required: false, default: false},
     autoGuide: {type: Boolean, required: false, default: false},
     autoFocus: {type: Boolean, required: false, default: false},
     stack: {type: Boolean, required: false, default: false},
@@ -31,6 +31,9 @@ const observation = new Schema({
     dithering: {type: Number, required: false},
     track: {type: Boolean, required: false},
     defocus: {type: Number, required: false},
+    frameKind: {type: String, enum: ['IMAGE', 'DARK', 'BIAS'] , default: 'IMAGE' , required: true},
+    darkFrames: {type: Number},
+    biasFrames: {type: Number}
 });
 
 module.exports = mongoose.model('Observation', observation)
