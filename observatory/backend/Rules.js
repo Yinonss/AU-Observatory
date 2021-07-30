5c38130e9663299c3e9aaea1f4dd7bc25da368f5
maor6 <maor.ovadia6@gmail.com>
2021-07-30 11:01:07 +0300

    Make rule service

--- observatory/backend/Rules.js
+++ observatory/backend/Rules.js
@@ -1,34 +1,66 @@
+const express = require('express')
+const app = express()
+const port = 5001
+
 var RuleEngine = require('node-rules')
 
-var R = new RuleEngine();
+app.use(express.json())
 
-/* Sample Rule to block a transaction if its below 500 */
-var rule = {
+var R = new RuleEngine()
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    next();
+});
+
+/* Creating Rules */
+var TargetNameRule = {
     "condition": function(R) {
         R.when(this.name === "");
     },
     "consequence": function(R) {
         this.result = false;
-        this.reason = "the name is empty";
+        this.reason = "target name is empty";
         R.stop();
     }
 };
-/* Creating Rule Engine instance and registering rule */
 
-
-R.register(rule);
-/* Fact with less than 500 as transaction, and this should be blocked */
-var fact = {
-    "name": "",
-    "application": "MOB2",
-    "transactionTotal": 400,
-    "cardType": "Credit Card"
+var RAAndDERule = {
+    "condition": function(R) {
+        R.when(this.ra === '' || this.dec === '');
+    },
+    "consequence": function(R) {
+        this.result = false;
+        this.reason = "Right Ascension or Declination are empty";
+        R.stop();
+    }
 };
 
-R.execute(fact, function(data) {
-    if (data.result) {
-        console.log("Valid transaction");
-    } else {
-        console.log("Blocked Reason:" + data.reason);
-    }
-});
+
+/* Apply the rules */
+R.register(TargetNameRule);
+R.register(RAAndDERule);
+
+
+
+// request rules for section 1
+app.post('/rules/1', (req, res) => {
+    let obj = req.body.target
+    console.log(obj)
+    R.execute(obj, function (data) {
+        if (data.result) {
+            console.log("valid")
+            res.send({isValid: data.result})
+        } else {
+            console.log("Blocked" + data.reason);
+            res.send({isValid: data.result, reason: data.reason})
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example rules listening at http://localhost:${port}`)
+})
