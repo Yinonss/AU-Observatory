8fae5e0c3d97c51bb5a6fc1e42dcebfd52c3a768
maor6 <maor.ovadia6@gmail.com>
2021-08-22 21:39:32 +0300

    Switch header and div

--- observatory/src/components/NavMenu.js
+++ observatory/src/components/NavMenu.js
@@ -4,16 +4,16 @@ import Menu from './../Styles/Menu.css'
 
 export default function NavMenu(){
     return (
-        <div className='main-menu'>
-            <header>
-                <nav className="navmenu">
-                    <li><NavLink to='/'>Home</NavLink></li>
-                    <li><NavLink to='/newplan'>New Plan</NavLink></li>
-                    <li><NavLink to='/planlist'>Dashboard</NavLink></li>
-                    <li><NavLink to='/guide'>Guide</NavLink></li>
-                    <li><NavLink to='/about'>About</NavLink></li>
-                </nav>
-            </header>
-        </div>
+        <header>
+            <div className='main-menu'>
+                    <nav className="navmenu">
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/newplan'>New Plan</NavLink></li>
+                        <li><NavLink to='/planlist'>PLANS LIST</NavLink></li>
+                        <li><NavLink to='/guide'>Guide</NavLink></li>
+                        <li><NavLink to='/about'>About</NavLink></li>
+                    </nav>
+            </div>
+        </header>
     );
 }
