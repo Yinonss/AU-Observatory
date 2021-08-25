e7eab34d8494fd9837699202f60cc0d09521bb87
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-08-25 08:51:40 +0300

    Add headline component to menu

--- observatory/src/components/NavMenu.js
+++ observatory/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {NavLink} from "react-router-dom";
 import Menu from './../Styles/Menu.css'
 
+
 export default function NavMenu(){
     return (
         <header>
@@ -9,7 +10,7 @@ export default function NavMenu(){
                     <nav className="navmenu">
                         <li><NavLink to='/'>Home</NavLink></li>
                         <li><NavLink to='/newplan'>New Plan</NavLink></li>
-                        <li><NavLink to='/planlist'>PLANS LIST</NavLink></li>
+                        <li><NavLink to='/planlist'>Plan List</NavLink></li>
                         <li><NavLink to='/guide'>Guide</NavLink></li>
                         <li><NavLink to='/about'>About</NavLink></li>
                     </nav>
