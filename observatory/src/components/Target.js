ed14b799e24020d686afcf90770bbcc2ddbd4c8e
DESKTOP-HLUJT3C\yinon <yinonss@hotmail.com>
2021-08-26 12:00:39 +0300

    Remove tooltip

--- observatory/src/components/Target.js
+++ observatory/src/components/Target.js
@@ -18,44 +18,6 @@ export default function Target(props) {
                     </button>
                     <h5>{props.name}</h5>
             </div>
-            <ReactTooltip id="targetDetails" place="right" props={props} >
-                <div>
-                    <table>
-                        <tr>
-                            <td>Name :</td>
-                            <td>{props.name}</td>
-                        </tr>
-                        <tr>
-                            <td>RA : </td>
-                            <td>{props.ra}</td>
-                        </tr>
-                        <tr>
-                            <td>Dec : </td>
-                            <td>{props.dec}</td>
-                        </tr>
-                        <tr>
-                            <td>Filter : </td>
-                            <td>{props.filter}</td>
-                        </tr>
-                        <tr>
-                            <td>Exposures : </td>
-                            <td>{props.exposures}</td>
-                        </tr>
-                        <tr>
-                            <td>Duration : </td>
-                            <td>{props.exposureTime}</td>
-                        </tr>
-                        <tr>
-                            <td>Start : </td>
-                            <td>{props.start}</td>
-                        </tr>
-                        <tr>
-                            <td>End : </td>
-                            <td>{props.end}</td>
-                        </tr>
-                    </table>
-                </div>
-            </ReactTooltip>
         </div>
     )
 }
